8baf7b5b0300b4c60574bc53d880535a8d3cc8c6
abi <65239234+boredabi@users.noreply.github.com>
2021-09-01 20:56:57 +0400

    Update bot_ver_final.py

--- bot_ver_final.py
+++ bot_ver_final.py
@@ -6,25 +6,28 @@ from pyautogui import pixelMatchesColor, mouseDown, mouseUp
 from time import sleep
 
 while 1:
-	starting_points = []
+	starting_points = [] #finding the starting point to get the  abscissa to check if the bridge is long enoguh
 	for i in range(793,811):
 		if pixelMatchesColor(i,679,(0,0,0)):
 			starting_points.append(i)
 	for n in range(820,1215,10): # finding the red point
 		if pixelMatchesColor(n,679,(253,10,1)):
 			break
-	y = (679-((n-2)-(starting_points[-1]))) #added -3 to compensate for the extra(10) shifiting
-	x = starting_points[-1]
+	#calculating the desired height of the bar
+	y = (679-((n-2)-(starting_points[-1]))) #added -2 to compensate for the extra(10) shifiting, added a 10 skip range to make it faster
+	x = starting_points[-1] #finding the maximum point to get the outermost edge
 	mouseDown()
 	while 1:
 		if pixelMatchesColor(x,y,(0,0,0)):
-			mouseUp()
+			mouseUp() #stopping the click once the bar reaches the desired height
 			break
+	
+	#compensating for the new columns to show up inorder to get accurate readings
 	if n <=860:
 		sleep(1.7)
 	elif n <=920:
 		sleep(1.8)
-	elif n <= 940: #aDD 880 conditon and sleep for 1.8 seconds if it doenst work
+	elif n <= 940: 
 		sleep(1.9)
 	elif n <= 1000:
 		sleep(2)
